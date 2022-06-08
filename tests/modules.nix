08ad01b1b88108e388c7b5a792c55e0b59fe02aa
Matt Schreiber <schreibah@gmail.com>
2022-06-08 08:41:12 -0400

    Add Nix flake checks
    that:
    
        1. Exercise `environment.persistence` by setting up multiple users
           and groups, multiple persistent storage directories, and multiple
           files and directories under those storage directories (some with
           overlapping destination paths), and then making assertions about
           the permissions on the source and destination paths, and
        2. Check that exceptional conditions result in assertion failures
           whose error messages match the expected patterns.
    
    Additionally, update to `install-nix-action` version 17 for easy flake
    support (no need to twiddle `extra-experimental-features` ourselves).

---
+++ tests/modules.nix
@@ -0,0 +1,75 @@
+{ modules ? [ ], nixpkgs, system, ... }:
+
+let
+  pkgs = nixpkgs.legacyPackages.${system};
+
+  inherit (nixpkgs.lib) any escapeShellArg nixosSystem runTests;
+
+  mkSystem = config: nixosSystem {
+    inherit system;
+    modules = modules ++ [
+      # slim down the config
+      { boot.isContainer = true; }
+      config
+    ];
+  };
+
+  search = pattern: str: (builtins.match ".*${pattern}.*" str) != null;
+
+  assertionsMatch = pattern: any (assertion: !assertion.assertion && search pattern assertion.message);
+
+  checkAssertionsMatch = pattern: config: {
+    expected = true;
+    expr = assertionsMatch pattern (mkSystem config).config.assertions;
+  };
+
+  # XXX remember that `runTests` only runs test cases when they are associated
+  # with an attribute name that starts with `test`!
+  tests = runTests {
+    testDuplicateRootDirectories = checkAssertionsMatch "The following directories were specified two or more[^a-z]*times" {
+      environment.persistence = {
+        "/abc".directories = [ "/same" ];
+        "/def".directories = [ "/same" ];
+      };
+    };
+
+    testDuplicateRootFiles = checkAssertionsMatch "The following files were specified two or more[^a-z]*times" {
+      environment.persistence = {
+        "/abc".files = [ "/same/file" ];
+        "/def".files = [ "/same/file" ];
+      };
+    };
+
+    testDuplicateUserDirectories = checkAssertionsMatch "The following directories were specified two or more[^a-z]*times" {
+      users.users.auser.isNormalUser = true;
+      environment.persistence = {
+        "/abc".users.auser.directories = [ "/same" ];
+        "/def".users.auser.directories = [ "/same" ];
+      };
+    };
+
+    testDuplicateUserFiles = checkAssertionsMatch "The following files were specified two or more[^a-z]*times" {
+      users.users.auser.isNormalUser = true;
+      environment.persistence = {
+        "/abc".users.auser.files = [ "/same/file" ];
+        "/def".users.auser.files = [ "/same/file" ];
+      };
+    };
+
+    testMissingNeededForBoot = checkAssertionsMatch "All filesystems used for persistent storage must[^a-z]*have the flag neededForBoot" {
+      fileSystems."/abc" = { fsType = "tmpfs"; neededForBoot = false; };
+      environment.persistence."/abc".directories = [ "/hello" ];
+    };
+
+    testHomeMismatch = checkAssertionsMatch "Users and home doesn't match" {
+      users.users.auser = { isNormalUser = true; home = "/althome/auser"; };
+      environment.persistence = {
+        "/abc".users.auser.files = [ "/a/file" ];
+      };
+    };
+  };
+in
+# Abort if `tests`(list containing failed tests) is not empty
+pkgs.runCommandNoCC "impermanence-module-tests" { } ''
+  ${pkgs.jq}/bin/jq 'if . == [] then . else halt_error(1) end' > "$out" <<<${escapeShellArg (builtins.toJSON tests)}
+''
