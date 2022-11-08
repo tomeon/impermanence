1e7cb1f44290fe58ec079098c9351eeca01d7221
Matt Schreiber <schreibah@gmail.com>
2022-11-08 16:31:31 -0500

    lib: harmonize path-munging functions
    As follows:
    
        1. Clean the concatenated paths in `dirListToPath` with `cleanPath`,
        2. Make `concatPaths` an alias of `dirListToPath`,
        3. Make `splitPath` a small wrapper over `concatPaths` (namely,
           split the result of `concatPaths [ list of paths ]` on the
           directory separator).
    
    BREAKING CHANGE: all three of `dirListToPath`, `concatPaths`, and
    `splitPath` are now liable to throw on ".." elements that cannot be
    eliminated lexically.

--- lib.nix
+++ lib.nix
@@ -1,15 +1,12 @@
 { lib }:
 let
   inherit (lib) filter concatMap concatStringsSep hasPrefix head
-    replaceStrings optionalString removePrefix foldl' elem;
+    replaceStrings removePrefix foldl' elem;
   inherit (lib.strings) sanitizeDerivationName;
 
   # ["/home/user/" "/.screenrc"] -> ["home" "user" ".screenrc"]
   splitPath = paths:
-    (filter
-      (s: builtins.typeOf s == "string" && s != "" && s != ".")
-      (concatMap (builtins.split "/") paths)
-    );
+    filter builtins.isString (builtins.split "/" (concatPaths paths));
 
   # Remove duplicate "/" elements, "/./", "foo/..", etc., from a path
   cleanPath = path:
@@ -28,15 +25,11 @@ let
       throw "illegal path traversal in `${path}`";
 
   # ["home" "user" ".screenrc"] -> "home/user/.screenrc"
-  dirListToPath = dirList: (concatStringsSep "/" dirList);
-
   # ["/home/user/" "/.screenrc"] -> "/home/user/.screenrc"
-  concatPaths = paths:
-    let
-      prefix = optionalString (hasPrefix "/" (head paths)) "/";
-      path = dirListToPath (splitPath paths);
-    in
-    prefix + path;
+  dirListToPath = dirList: cleanPath (concatStringsSep "/" dirList);
+
+  # Alias of dirListToPath
+  concatPaths = dirListToPath;
 
   sanitizeName = name:
     replaceStrings
